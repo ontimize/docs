7b1f81cb6318f51df2b3af2b90732ceb9a6234f6
pmkirsten <pmkirsten@users.noreply.github.com>
2023-12-21 08:51:01 +0000

    jekyll build from Action 49c9953331faf716efefe51591d15c22a5138aea

--- v1/assets/js/just-the-docs.js
+++ v1/assets/js/just-the-docs.js
@@ -67,7 +67,7 @@ function disableHeadStyleSheet() {
 
 function initSearch() {
   var request = new XMLHttpRequest();
-  request.open('GET', '/docs/v1/assets/js/search-data.json', true);
+  request.open('GET', '/newdocs/v1/assets/js/search-data.json', true);
 
   request.onload = function(){
     if (request.status >= 200 && request.status < 400) {
@@ -446,7 +446,7 @@ jtd.getTheme = function() {
 
 jtd.setTheme = function(theme) {
   var cssFile = document.querySelector('[rel="stylesheet"]');
-  cssFile.setAttribute('href', '/docs/v1/assets/css/just-the-docs-' + theme + '.css');
+  cssFile.setAttribute('href', '/newdocs/v1/assets/css/just-the-docs-' + theme + '.css');
 }
 
 // Note: pathname can have a trailing slash on a local jekyll server
